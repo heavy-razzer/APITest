74036c3e1292c0ff99af879fc42c49d3d0ca6782
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-21 15:11:31 +0200

    Use JSONObject

---
+++ src/test/java/httpCommands/HttpCommands.java
@@ -0,0 +1,27 @@
+package httpCommands;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class HttpCommands {
+
+    public static JSONObject getResponceJSON(String path) {
+
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(path);
+            HttpResponse response = httpClient.execute(request);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                return null;
+            }
+            String json_string = EntityUtils.toString(response.getEntity());
+            return new JSONObject(json_string);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
