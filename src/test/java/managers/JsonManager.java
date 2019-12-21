4f4b2dcbf32774a34d0af790a28c35417f102ebd
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-21 17:49:11 +0200

    Move comparison function to separate class from tets class

--- src/test/java/managers/JsonManager.java
+++ src/test/java/managers/JsonManager.java
@@ -8,7 +8,38 @@ import java.util.*;
 
 public class JsonManager {
 
-    public static Map<Object, Object> jsonToMap(JSONObject json) throws JSONException {
+    public static boolean compareJSons(JSONObject js1, JSONObject js2) {
+
+        boolean result = false;
+
+        // Compare jsons string as is
+        if (js1.toString().equals(js2.toString()))
+            return true;
+
+        // Build map from json
+        Map jsMap1 = jsonToMap(js1);
+        Map jsMap2 = jsonToMap(js2);
+
+        // If root items in maps is not the same, break: jsons are different for sure
+        if (jsMap1.entrySet().toArray().length != jsMap2.entrySet().toArray().length) return false;
+
+        Object[] js1Array = jsMap1.entrySet().toArray();
+        Object[] js2Array = jsMap2.entrySet().toArray();
+
+        System.out.println("---");
+        for (Object arrayItem : js1Array) {
+            System.out.println(arrayItem.toString());
+        }
+
+        System.out.println("---");
+        for (Object arrayItem : js2Array) {
+            System.out.println(arrayItem.toString());
+        }
+
+        return result;
+    }
+
+    private static Map<Object, Object> jsonToMap(JSONObject json) throws JSONException {
 
         Map<Object, Object> retMap = new TreeMap<>();
         if (json != JSONObject.NULL) {
--- src/test/java/myTest.java
+++ src/test/java/myTest.java
@@ -1,12 +1,9 @@
-import httpCommands.HttpCommands;
 import managers.FileManager;
 import org.json.JSONObject;
 import org.junit.Test;
 
-import java.util.Map;
-
-import static managers.JsonManager.convertToTreeMap;
-import static managers.JsonManager.jsonToMap;
+import static httpCommands.HttpCommands.getResponceJSON;
+import static managers.JsonManager.compareJSons;
 
 public class myTest {
 
@@ -26,8 +23,8 @@ public class myTest {
             if (stringFromFile1 == null || stringFromFile2 == null)
                 break;
 
-            JSONObject js1 = HttpCommands.getResponceJSON(stringFromFile1);
-            JSONObject js2 = HttpCommands.getResponceJSON(stringFromFile2);
+            JSONObject js1 = getResponceJSON(stringFromFile1);
+            JSONObject js2 = getResponceJSON(stringFromFile2);
             String json1 = js1.toString();
             String json2 = js2.toString();
             System.out.println(json1);
@@ -38,56 +35,4 @@ public class myTest {
             System.out.println("===============");
         }
     }
-
-    public boolean compareJSons(JSONObject js1, JSONObject js2) {
-
-        boolean result = false;
-
-        // Compare string jsons as is
-        if (js1.toString().equals(js2.toString()))
-            return true;
-
-        // Build map from json
-        Map jsMap1 = jsonToMap(js1);
-        Map jsMap2 = jsonToMap(js2);
-
-        Map treeMap1 = convertToTreeMap(jsMap1);
-        Map treeMap2 = convertToTreeMap(jsMap2);
-
-        //if (jsMap1.entrySet().toArray().length != jsMap2.entrySet().toArray().length) return false;
-
-        Object[] js1Array = jsMap1.entrySet().toArray();
-        Object[] js2Array = jsMap2.entrySet().toArray();
-
-        Object[] tree1Array = treeMap1.entrySet().toArray();
-        Object[] tree2Array = treeMap2.entrySet().toArray();
-
-        // If number of items on first level in jsons are not same, then return false
-        //if (js1Array.length != js2Array.length) return false;
-
-        //Arrays.sort(js1Array);
-        //Arrays.sort(js2Array);
-
-        System.out.println("---");
-        for (Object arrayItem : js1Array) {
-            System.out.println(arrayItem.toString());
-        }
-        System.out.println(" ");
-        for (Object arrayItem : tree1Array) {
-            System.out.println(arrayItem.toString());
-        }
-
-        System.out.println("---");
-        for (Object arrayItem : js2Array) {
-            System.out.println(arrayItem.toString());
-        }
-        System.out.println(" ");
-        for (Object arrayItem : tree2Array) {
-            System.out.println(arrayItem.toString());
-        }
-
-        return result;
-    }
-
-
 }
