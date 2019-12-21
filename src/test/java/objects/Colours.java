faa00e28a90c9aa3de9f1eb399d82e9892b9c0f7
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-21 13:52:10 +0200

    Log functions

---
+++ src/test/java/objects/Colours.java
@@ -0,0 +1,23 @@
+package objects;
+
+/*
+Colours used for highlightning test on log
+ */
+public enum Colours {
+    DEFAULT("\u001B[0m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    BLUE("\u001B[34m"),
+    CYAN("\u001B[36m"),
+    PURPLE("\u001B[35m");
+
+    private String colour;
+
+    Colours(String color) {
+        this.colour = color;
+    }
+
+    public String getValue() {
+        return colour;
+    }
+}
