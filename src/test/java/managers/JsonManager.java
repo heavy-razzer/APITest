0e7b3609beee684e6f5254d289c00ce9edd06b18
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-21 17:45:44 +0200

    Optimise code

--- src/test/java/managers/JsonManager.java
+++ src/test/java/managers/JsonManager.java
@@ -5,40 +5,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class JsonManager {
 
-    public static <K, V> Map<K, V> convertToTreeMap(Map<K, V> hashMap) {
-        return hashMap
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue,
-                         newValue)
-                                -> newValue,
-                        TreeMap::new));
-    }
+    public static Map<Object, Object> jsonToMap(JSONObject json) throws JSONException {
 
-    public static Map<String, Object> jsonToMap(JSONObject json) throws JSONException {
-        Map<String, Object> retMap = new HashMap<>();
-
+        Map<Object, Object> retMap = new TreeMap<>();
         if (json != JSONObject.NULL) {
             retMap = toMap(json);
         }
         return retMap;
     }
 
-    private static Map<String, Object> toMap(JSONObject object) throws JSONException {
-        Map<String, Object> map = new HashMap<>();
+    private static Map<Object, Object> toMap(JSONObject object) throws JSONException {
 
+        Map<Object, Object> map = new TreeMap<>();
         Iterator<String> keysItr = object.keys();
         while (keysItr.hasNext()) {
             String key = keysItr.next();
             Object value = object.get(key);
-
             if (value instanceof JSONArray) {
                 value = toList((JSONArray) value);
             } else if (value instanceof JSONObject) {
@@ -50,6 +35,7 @@ public class JsonManager {
     }
 
     private static List<Object> toList(JSONArray array) throws JSONException {
+
         List<Object> list = new ArrayList<>();
         for (int i = 0; i < array.length(); i++) {
             Object value = array.get(i);
