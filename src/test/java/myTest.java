eeb778156158d0bd41918c9ffdb4e21ff7111b58
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-21 18:53:38 +0200

    Add comments

--- src/test/java/myTest.java
+++ src/test/java/myTest.java
@@ -15,17 +15,21 @@ public class myTest {
         String fileOneName = "file1full.txt";
         String fileTwoName = "file2full.txt";
 
+        // Load file reader
         FileManager file1 = new FileManager(fileOneName);
         FileManager file2 = new FileManager(fileTwoName);
 
         String stringFromFile1;
         String stringFromFile2;
 
+        // Until end of file is not reached
         while (true) {
 
+            // Try to read one string from each file
             stringFromFile1 = file1.getNextLine();
             stringFromFile2 = file2.getNextLine();
 
+            // Check if not EOF
             if (stringFromFile1 == null) {
                 System.out.println("End of file '" + fileOneName + "'");
                 break;
@@ -36,6 +40,7 @@ public class myTest {
                 break;
             }
 
+            // Execute http commands
             JSONObject js1 = getResponse(stringFromFile1);
             JSONObject js2 = getResponse(stringFromFile2);
 
@@ -43,6 +48,7 @@ public class myTest {
             //System.out.println(js1.toString());
             //System.out.println(js2.toString());
 
+            // Compare responses
             if (compareJSons(js1, js2)) {
                 System.out.println(Colours.CYAN.getValue() + stringFromFile1 + Colours.GREEN.getValue() + " equals " + Colours.CYAN.getValue() + stringFromFile2 + Colours.DEFAULT.getValue());
             } else {
