39f139e64973f782a921bb891ce1c9ba93adcddd
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-22 17:45:16 +0200

    More tests added

--- src/test/java/test/frameworkTest.java
+++ src/test/java/test/frameworkTest.java
@@ -18,8 +18,6 @@ public class frameworkTest {
     @Test
     public void shouldReturnTrueWhenUseTwoIdenticalJSONS() {
 
-        System.out.println("Should Return True When Use Two Identical JSONS");
-
         JSONObject js1 = new JSONObject();
 
         js1.put("first", "qwe");
@@ -31,8 +29,6 @@ public class frameworkTest {
 
     @Test
     public void shouldReturnFalseWhenUseTwoDifferentJSONS() {
-
-        System.out.println("Should Return False When Use Two Different JSONS");
 
         JSONObject js1 = new JSONObject();
         JSONObject js2 = new JSONObject();
@@ -51,8 +47,6 @@ public class frameworkTest {
     @Test
     public void shouldReturnTrueWhenUseJSONWithDifferentFieldsOrder() {
 
-        System.out.println("Should Return True When Use JSON With Different Fields Order");
-
         JSONObject js1 = new JSONObject();
         JSONObject js2 = new JSONObject();
 
@@ -69,8 +63,6 @@ public class frameworkTest {
 
     @Test
     public void shouldReturnTrueWhenUseJSONFromMapWithDifferentFieldsOrder() {
-
-        System.out.println("Should Return True When Use JSON From Map With Different Fields Order");
 
         // HashMap is fastest unordered map
         Map<String, String> map1 = new HashMap<>();
@@ -90,8 +82,6 @@ public class frameworkTest {
 
     @Test
     public void shouldReturnFalseWhenOneJSONIsNull() {
-
-        System.out.println("Should Return False When One JSON Is Null");
 
         JSONObject js1 = new JSONObject();
         JSONObject js2 = null;
@@ -115,8 +105,6 @@ public class frameworkTest {
     @Test
     public void shouldReturnFalseWhenOneJSONIsEmpty() {
 
-        System.out.println("Should Return False When One JSON Is Empty");
-
         JSONObject js1 = new JSONObject("{}");
         JSONObject js2 = new JSONObject();
 
@@ -129,8 +117,6 @@ public class frameworkTest {
 
     @Test
     public void shouldReturnFalseWhenOneResponseIsNotJSON() {
-
-        System.out.println("Should Return False When Response Is Not JSON");
 
         JSONObject js1 = new JSONObject();
 
@@ -145,8 +131,6 @@ public class frameworkTest {
 
     @Test
     public void shouldReturnTrueWhenJSONsHasMultiLevelStructure() {
-
-        System.out.println("Should Return True When JSONs Has Multi Level Structure");
 
         Map<Object, Object> map1 = new HashMap<>();
         Map<Object, Object> map2 = new HashMap<>();
@@ -171,8 +155,6 @@ public class frameworkTest {
     @Test
     public void shouldReturnFalseWhenOneJSONHasAdditionalItem() {
 
-        System.out.println("Should Return False When One JSON Has Additional Item");
-
         JSONObject js1 = new JSONObject();
         JSONObject js2 = new JSONObject();
 
@@ -189,7 +171,7 @@ public class frameworkTest {
     }
 
     @Test
-    public void shouldReturnFailWhenJSONsHasSameItemsWithDifferentValues() {
+    public void shouldReturnFalseWhenJSONsHasSameItemsWithDifferentValues() {
 
         JSONObject js1 = new JSONObject();
         JSONObject js2 = new JSONObject();
@@ -206,7 +188,7 @@ public class frameworkTest {
     }
 
     @Test
-    public void shouldReturnFailWhenJSONsHasDifferentItemsWithSameValues() {
+    public void shouldReturnFalseWhenJSONsHasDifferentItemsWithSameValues() {
 
         JSONObject js1 = new JSONObject();
         JSONObject js2 = new JSONObject();
@@ -219,6 +201,58 @@ public class frameworkTest {
         js2.put("222", "asd");
         js2.put("333", "zxc");
 
-        Assert.assertFalse("FAILED: Returned TRUE when one JSON has different item names, but with same values", compareJSons(js1, js2));
+        Assert.assertFalse("FAILED: Returned TRUE when JSONs has different item names, but with same values", compareJSons(js1, js2));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenJSONsHasDifferentValueTypesStringAndInt() {
+
+        JSONObject js1 = new JSONObject();
+        JSONObject js2 = new JSONObject();
+
+        js1.put("first", "111");
+
+        js2.put("first", 111);
+
+        Assert.assertFalse("FAILED: Returned TRUE when JSONs has same fields, but with different value types (string and int)", compareJSons(js1, js2));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenJSONsHasDifferentValueTypesStringAndBool() {
+
+        JSONObject js1 = new JSONObject();
+        JSONObject js2 = new JSONObject();
+
+        js1.put("first", "true");
+
+        js2.put("first", true);
+
+        Assert.assertFalse("FAILED: Returned TRUE when JSONs has same fields, but with different value types (string and boolean)", compareJSons(js1, js2));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenJSONsHasDifferentArraysAsValues() {
+
+        String[] array1 = new String[]{"aaa", "bbb", "ccc", "bbb"};
+        String[] array2 = new String[]{"qqq", "www", "eee", "ddd"};
+
+        JSONObject js1 = new JSONObject();
+        JSONObject js2 = new JSONObject();
+
+        js1.put("first", array1);
+
+        js2.put("first", array2);
+
+        Assert.assertFalse("FAILED: Returned TRUE when JSONs has same fields, but with different arrays in it", compareJSons(js1, js2));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenJSONsHasSameArraysAsValues() {
+
+        String[] array1 = new String[]{"aaa", "bbb", "ccc", "bbb"};
+        JSONObject js1 = new JSONObject();
+        js1.put("first", array1);
+
+        Assert.assertFalse("FAILED: Returned TRUE when JSONs has same fields with same arrays", compareJSons(js1, js1));
     }
 }
